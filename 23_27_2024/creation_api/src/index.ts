84df6ee4d5720a60cbf0dc7ee3edcc8ca1689eae
Graff Alexandre <alexgraff67@gmail.com>
2024-09-25 17:43:24 +0200

    ajout du fichier routerFile.ts

--- 23_27_2024/creation_api/src/index.ts
+++ 23_27_2024/creation_api/src/index.ts
@@ -1,17 +1,11 @@
 import express from 'express';
-import repoControllers from './repos/repos.controllers';
-import langsControllers from './langs/langs.controllers';
-import statusControllers from './status/status.controllers';
-import langByRepoControllers from './lang_by_repo/lang_by_repo.controllers';
+import routerfile from './routerFile';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/repos', repoControllers);
-app.use('/langs', langsControllers);
-app.use('/status', statusControllers);
-app.use('/lang_by_repo', langByRepoControllers);
+app.use('/api', routerfile);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
