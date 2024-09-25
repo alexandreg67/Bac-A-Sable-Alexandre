028ff1bb479ee6f975307fc9dee3210921911766
Graff Alexandre <alexgraff67@gmail.com>
2024-09-25 12:16:09 +0200

    utilisation de la commande exec

--- 23_27_2024/github_repo_modelisation/index.ts
+++ 23_27_2024/github_repo_modelisation/index.ts
@@ -1,31 +1,25 @@
 import * as fs from 'fs';
-
-const GIT_API_URL = 'https://api.github.com/users/alexandreg67/repos';
+import { exec } from 'child_process';
 
 async function fetchRepos() {
 	try {
-		const response = await fetch(GIT_API_URL);
-		const repos = await response.json();
+		// exec(command, callback);
+		exec('gh repo list alexandreg67', (error, stdout, stderr) => {
+			try {
+				const repos = stdout;
+				console.log('Liste des repositories :', stdout);
 
-		const reposList = repos.map((repo: any) => {
-			return {
-				name: repo.name,
-			};
-		});
-
-		fs.writeFile(
-			'repos-data.json',
-			JSON.stringify(reposList, null, 2),
-			(err) => {
-				if (err) {
-					console.error("Erreur lors de l'écriture dans le fichier", err);
-				} else {
-					console.log('Les données ont été sauvegardées dans repos-data.json');
-				}
+				fs.writeFile('raw.json', JSON.stringify(repos, null, 2), (err) => {
+					if (err) {
+						console.error("Erreur lors de l'écriture dans le fichier", err);
+					} else {
+						console.log('Les données ont été sauvegardées dans raw.json');
+					}
+				});
+			} catch (parseError) {
+				console.error('Erreur lors du parsing de la sortie gh :', parseError);
 			}
-		);
-
-		return reposList;
+		});
 	} catch (error) {
 		console.error('Erreur lors de la récupération des repositories :', error);
 	}
