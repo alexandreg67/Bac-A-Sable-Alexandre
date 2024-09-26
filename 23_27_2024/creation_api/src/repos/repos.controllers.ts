4eca92e82fd964cd4236d2a4ebb77005993a51bb
Graff Alexandre <alexgraff67@gmail.com>
2024-09-26 16:34:39 +0200

    ajout de la route delete et put de repos.controllers.ts

--- 23_27_2024/creation_api/src/repos/repos.controllers.ts
+++ 23_27_2024/creation_api/src/repos/repos.controllers.ts
@@ -1,9 +1,11 @@
 import express, { Response, Request, NextFunction } from 'express';
-import repos from '../../data/repos.json';
+// import repos from '../../data/repos.json';
 import Joi from 'joi';
 import { Repo } from './repo.type';
 
 const repoControllers = express.Router();
+
+let repos: Repo[] = require('../../data/repos.json');
 
 // règles de validation pour le body de la requête
 const schema = Joi.object({
@@ -54,6 +56,53 @@ repoControllers.post('/', validateRepo, (req: Request, res: Response) => {
 	(repos as Repo[]).push(newRepo);
 	// on renvoie le repo ajouté
 	res.status(201).json(newRepo);
+});
+
+repoControllers.put('/:id', validateRepo, (req: Request, res: Response) => {
+	const repo = (repos as Repo[]).find((rep) => rep.id === req.params.id);
+
+	if (repo) {
+		// typage du body de la requête
+		const updatedRepo: Repo = req.body;
+		// on met à jour le repo dans le tableau en mémoire
+		Object.assign(repo, updatedRepo);
+		// on renvoie le repo mis à jour
+		res.status(200).json(updatedRepo);
+	} else {
+		// on renvoie une erreur 404 si le repo n'est pas trouvé
+		res.sendStatus(404);
+	}
+});
+
+repoControllers.delete('/:id', (req: Request, res: Response) => {
+	// on cherche le repo dans le tableau en mémoire
+	const repo = repos.find((rep) => rep.id === req.params.id);
+
+	if (!repo) {
+		return res.status(404).json({
+			message: `Le repo avec l'id ${req.params.id} n'a pas été trouvé.`,
+		});
+	}
+
+	// on cherche le repo dans le tableau en mémoire
+	// const repoIndex = (repos as Repo[]).findIndex(
+	// 	(rep) => rep.id === req.params.id
+	// );
+
+	repos = repos.filter((rep) => rep.id !== req.params.id);
+
+	// if (repoIndex !== -1) {
+	// 	(repos as Repo[]).splice(repoIndex, 1);
+	// 	res.sendStatus(204);
+	// } else {
+	// 	res.sendStatus(404);
+	// }
+
+	res
+		.status(200)
+		.json({
+			message: `Le repo avec l'id ${req.params.id} a été supprimé avec succès.`,
+		});
 });
 
 export default repoControllers;
