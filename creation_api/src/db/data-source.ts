101b27acb2642894f196a7f9e2b7a103b70dc0be
Alex <alexandre.ag.67@gmail.com>
2024-11-04 11:21:19 +0100

    modification des variables d'environnement et du datasource

--- creation_api/src/db/data-source.ts
+++ creation_api/src/db/data-source.ts
@@ -6,8 +6,7 @@ import { Lang } from "../langs/lang.entities";
 import { Comment } from "../comments/comment.entities";
 
 dotenv.config();
-const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
-  process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 // On crée une instance de DataSource
 // export const AppDataSource = new DataSource({
@@ -20,11 +19,11 @@ const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST, // Nom du conteneur PostgreSQL
+  host: process.env.DB_HOST, // Nom du conteneur PostgreSQL
   port: 5432, // Port par défaut de PostgreSQL
-  username: process.env.POSTGRES_USER, // Nom d'utilisateur de PostgreSQL
-  password: process.env.POSTGRES_PASSWORD, // Mot de passe PostgreSQL
-  database: process.env.POSTGRES_DB, // Nom de la base de données
+  username: process.env.DB_USER, // Nom d'utilisateur de PostgreSQL
+  password: process.env.DB_PASSWORD, // Mot de passe PostgreSQL
+  database: process.env.DB_NAME, // Nom de la base de données
   synchronize: true, // Pour synchroniser les entités
   // logging: false,
   entities: [Repo, Status, Lang, Comment],
